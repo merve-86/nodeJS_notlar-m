e576456664a567288c64325e19a93b1853c424d2
merve <merveaydin86@hotmail.com>
2024-07-01 21:27:39 +0300

    nodejsServer

---
+++ 06_NodejsServer/index.js
@@ -0,0 +1,32 @@
+"use strict"
+
+/*--------------------------------------------------*
+
+     NODEJS
+
+/*--------------------------------------------------*/
+
+//HHTPSERVER:
+
+const http = require('node:http') 
+
+/*--------------------------------------------------*/
+
+//http.createServer((request, response) => {...})
+
+// const app = http.createServer((request, response) => {
+//     response.end('Hello World')
+// })
+
+// app.listen(8000, () => console.log('Server started: http://127.0.0.1:8000'))
+
+/*--------------------------------------------------*/
+// const app = http.createServer((req, res) => {
+//     //console.log(req)
+//     console.log(res)
+// }) 
+
+// app.listen(8000, () => console.log('http://127.0.0.1:8000')) 
+
+/*--------------------------------------------------*/
+
