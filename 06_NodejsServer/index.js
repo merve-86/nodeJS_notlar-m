164c2961448f8ec2bd9538986e682bb544cdb7d7
merve <merveaydin86@hotmail.com>
2024-07-03 10:40:42 +0300

    thunder

--- 06_NodejsServer/index.js
+++ 06_NodejsServer/index.js
@@ -1,32 +1,130 @@
-"use strict"
+"use strict";
+/* -------------------------------------- *
 
-/*--------------------------------------------------*
+        NODEJS
 
-     NODEJS
+/* -------------------------------------- */
+// HTTPSERVER:
 
-/*--------------------------------------------------*/
+//const http = require("node:http"); // BuiltIn Module.
 
-//HHTPSERVER:
+/* -------------------------------------- *
 
-const http = require('node:http') 
+// http.createServer( (request, response) => { ... } )
 
-/*--------------------------------------------------*/
+const app = http.createServer((request, response) => {
 
-//http.createServer((request, response) => {...})
+    response.end('Hello World')
+    console.log('Console Print')
 
-// const app = http.createServer((request, response) => {
-//     response.end('Hello World')
-// })
+})
 
-// app.listen(8000, () => console.log('Server started: http://127.0.0.1:8000'))
+// Default server domain (local domain) = localhost
+// Default server IP (local IP) = 127.0.0.1
+app.listen(8000, () => console.log('Server started: http://127.0.0.1:8000'))
 
-/*--------------------------------------------------*/
-// const app = http.createServer((req, res) => {
-//     //console.log(req)
-//     console.log(res)
-// }) 
+/* -------------------------------------- *
 
-// app.listen(8000, () => console.log('http://127.0.0.1:8000')) 
+const app = http.createServer((req, res) => {
 
-/*--------------------------------------------------*/
+    // console.log(req)
+    // console.log(res)
+    // console.log(req.url)
 
+    if (req.url == '/') {
+
+        res.end('Main Page')
+
+    } else if (req.url == '/second') {
+
+        res.end('second page')
+
+    } else {
+
+        res.end('any page')
+    }
+
+})
+
+app.listen(8000, () => console.log('http://127.0.0.1:8000'))
+
+/* -------------------------------------- *
+
+const app = http
+  .createServer((req, res) => {
+    if (req.url == "/api") {
+      // res.end('API') // res.end işlemi blocklar. devam eden kodlar çalışmaz.
+
+      // res.write('Yazı-1')
+      // res.write('Yazı-2')
+      // res.write('Yazı-3')
+      // res.write('Yazı-4')
+      // res.end()
+
+      if (req.method == "GET") {
+        // setHeader (single headers)
+        res.setHeader("title", "value");
+        // writeHead(statusCode, {  multi headers })
+        // Sonda yeralması gereken methodlardandır.
+        // res.writeHead(400, {
+        //     'content-encoding': 'utf-8',
+        //     'multi-headers': 'test',
+        // })
+        // writeHead'de 2. parametre olarak statusMessage gönderebiliriz:
+        res.writeHead(400, "Hatali Islem", {
+          "content-encoding": "utf-8",
+          "multi-headers": "test",
+        });
+
+        const obj = {
+          result: true,
+          message: "Hello World",
+        };
+        res.write(JSON.stringify(obj));
+
+        // En sonda yer almalıdır:
+        res.end();
+      } else {
+        res.end("Wrong Method");
+      }
+    } else {
+      res.end("HTML");
+    }
+  })
+  .listen(8000, () => console.log("http://127.0.0.1:8000"));
+
+/* -------------------------------------- */
+/* -------------------------------------- */
+/* -------------------------------------- */
+
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+  if (req.url == "/api") {
+    // res.end("API");
+
+    // res.write('yazi-1')
+    // res.write('yazi-2')
+    // res.write('yazi-3')
+    // res.end()
+    //console.log(req.method)
+
+    if (req.method == "GET") {
+      const obj = {
+        result: true,
+        message: "Hello World",
+      };
+
+      res.write(JSON.stringify(obj));
+      res.end();
+      
+    } else {
+      res.end("Wrong Method");
+    }
+
+  } else {
+    res.end("HTML");
+  }
+});
+
+app.listen(8000, () => console.log("http://127.0.0.1:8000"));
