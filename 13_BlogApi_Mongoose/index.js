1ad41649f8214bd1f68d0a992388417ed7cd662c
merve <merveaydin86@hotmail.com>
2024-07-13 14:03:19 +0300

    up

--- 13_BlogApi_Mongoose/index.js
+++ 13_BlogApi_Mongoose/index.js
@@ -10,15 +10,29 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
-
+// Accept JSON:
 app.use(express.json());
+
+// DB CONNECTION:
+// const dbConnection = require('./src/dbConnection')
+// dbConnection()
+require("./src/dbConnection")();
+
+// Catch error from async:
+require("express-async-errors");
+
+/* ------------------------------------------------------- */
 
 app.all("/", (req, res) => {
   res.send("WELCOME TO BLOG API");
 });
 
-// continue from here...
+/* ------------------------------------------------------- */
+// Routes:
 
+app.use("/blog", require("./src/routes/blogRouter"));
+
+/* ------------------------------------------------------- */
 // Catch Errors:
 app.use(require("./src/errorHandler"));
 
--- 13_BlogApi_Mongoose/src/dbConnection.js
+++ 13_BlogApi_Mongoose/src/dbConnection.js
@@ -2,14 +2,18 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+// Mongoose ODM
+// $ npm i mongoose
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const dbConnection = () => {
+  // mongoose.connect('mongodb://localhost:27017/blogAPI')
+  // mongoose.connect(process.env?.MONGODB)
+  mongoose
+    .connect(process.env?.MONGODB || "mongodb://localhost:27017/blogAPI")
+    .then(() => console.log("* DB Connected."))
+    .catch(() => console.log("* DB Not Connected."));
+};
 
-    mongoose
-      .connect("mongodb://localhost:27017/")
-      .then(() => console.log("* DB Connected"))
-      .catch(() => console.log("* DB Not Connected"))
-
-}
+module.exports = dbConnection;
