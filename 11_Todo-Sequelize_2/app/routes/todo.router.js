67ced40e5cdd62990751df242dfd749537ae4715
merve <merveaydin86@hotmail.com>
2024-07-17 21:22:02 +0300

    up

--- 11_Todo-Sequelize_2/app/routes/todo.router.js
+++ 11_Todo-Sequelize_2/app/routes/todo.router.js
@@ -27,8 +27,13 @@ const todo = require("../controllers/todo.controller");
 
 /* ------------------------------------------------------- */
 
-router.route("/").get(todo.list).post(todo.create);
+router.route("/")
+.get(todo.list)
+.post(todo.create);
 
-router.route("/:id").get(todo.read).put(todo.update).delete(todo.delete);
+router.route("/:id")
+.get(todo.read)
+.put(todo.update)
+.delete(todo.delete);
 
 module.exports = router;
