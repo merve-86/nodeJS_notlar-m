4d3cf136c149935233e67bdedeb84839fe39fe79
merve <merveaydin86@hotmail.com>
2024-07-18 22:15:17 +0300

    up

---
+++ 15_BlogApi_SessionCookies/src/routes/authRouter.js
@@ -0,0 +1,19 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const router = require("express").Router();
+
+// Call Controller:
+const { auth } = require("../controllers/authController");
+
+/* ------------------------------------------------------- */
+
+// URL: /auth ->
+
+router.post("/login", auth.login);
+router.all("/logout", auth.logout);
+
+/* ------------------------------------------------------- */
+module.exports = router;
