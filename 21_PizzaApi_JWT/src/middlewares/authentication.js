4c0c0212c6dd268af980abc94d46897775f7c59f
merve <merveaydin86@hotmail.com>
2024-07-31 22:17:43 +0300

    up

---
+++ 21_PizzaApi_JWT/src/middlewares/authentication.js
@@ -0,0 +1,36 @@
+"use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+
+const Token = require("../models/token");
+const jwt = require('jsonwebtoken')
+
+module.exports = async (req, res, next) => {
+
+  req.user = null
+
+  const auth = req.headers?.authorization; // Token ...tokenKey...
+  const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...']
+
+  if (tokenKey) {
+    if (tokenKey[0] == "Token") {
+
+      const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+        "userId"
+      );
+      req.user = tokenData ? tokenData.userId : false;
+    } else if (tokenKey[0] == "Bearer") {
+
+      jwt.verify(tokenKey[1], process.env.ACCESS_KEY, function (error, accessData) {
+
+        // console.log(accessData)
+        req.user = accessData ? accessData : null 
+        
+
+      })
+      
+    }
+  }
+  next();
+};
