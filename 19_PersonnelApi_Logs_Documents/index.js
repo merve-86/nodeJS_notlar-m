b1b91245e03ad1206ded55d4ae934161599f8361
merve <merveaydin86@hotmail.com>
2024-07-31 10:32:26 +0300

    up

--- 19_PersonnelApi_Logs_Documents/index.js
+++ 19_PersonnelApi_Logs_Documents/index.js
@@ -33,22 +33,19 @@ app.use(
     //   }
   })
 );
-const morgan = require("morgan");
-// app.use(
-//   morgan(
-//     'TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'
-//   )
-// );
-const fs = require("node:fs");
-const now = new Date();
-//console.log(now, typeof now)
-const today = now.toISOString().split("T")[0];
-//console.log(today, typeof today)
-app.use(
-  morgan("combined", {
-    stream: fs.createWriteStream(`./logs/${today}.log`, { flags: "a+" }),
-  })
-);
+
+
+/* ------------------------------------------------------- */
+
+// DOCUMENTATION:
+// $ npm i swagger-autogen # JSON creator
+// $ npm i swagger-ui-express
+// $ npm i redoc-express
+
+
+/* ------------------------------------------------------- */
+app.use(require('./src/middlewares/logger'))
+
 // Authentication Middleware:
 app.use(require("./src/middlewares/authentication"));
 // res.getModelList():
