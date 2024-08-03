f3ca22b035cefc73b1da5c830f48f3e5306269c5
merve <merveaydin86@hotmail.com>
2024-08-03 21:01:28 +0300

    feat:created helpers and sync

--- 23_RentAcar/index.js
+++ 23_RentAcar/index.js
@@ -56,7 +56,7 @@ app.all("/", (req, res) => {
 });
 
 // Routes:
-app.use('/', require("./src/routes/"));
+app.use("/", require("./src/routes/"));
 
 /* ------------------------------------------------------- */
 
@@ -68,4 +68,4 @@ app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
-// require('./src/helpers/sync')()
+// require("./src/helpers/sync")();
